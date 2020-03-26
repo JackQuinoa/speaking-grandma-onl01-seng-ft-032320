ac609b369f1e9ce4d8248a64853d72fbfdfef36c
cultured-firmware-2832 <jquinoa208@gmail.com>
2020-03-26 18:42:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,10 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma
+  if "speak_to_grandma == .downcase"
+    puts ""
+    elsif
+  else
+end
