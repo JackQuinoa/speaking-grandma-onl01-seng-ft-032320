d52c8cda7cb24019ebbb05b74f86ee57b750b7ab
cultured-firmware-2832 <jquinoa208@gmail.com>
2020-03-26 18:49:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(string)
   if string == string.downcase
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
     elsif string == string.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else string == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMKPIN"
+    return "I LOVE YOU TOO PUMKPIN"
 end
 
